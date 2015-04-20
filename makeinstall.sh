d1fb174e7018472a12e3988ee808723a1f9564f5
阿宝 <talent518@yeah.net>
2015-04-20 18:21:47 +0000

    解决mysql连接不能即时关闭问题。
    git-svn-id: svn://127.0.0.1/mod_dump_mysql/trunk@5 2f7d24ea-dbb6-b042-b841-5bba3d118ff9

--- makeinstall.sh
+++ makeinstall.sh
@@ -4,7 +4,7 @@ lampp stopapache
 
 rm -rf .libs *.o *.slo *.lo *.la
 
-apxs -c -L/opt/lampp/lib -I/opt/lampp/include -lmysqlclient -lm -lz mod_dump_mysql.c && \
+apxs -c -L/opt/lampp/lib -I/opt/lampp/include -lmysqlclient mod_dump_mysql.c && \
 apxs -i mod_dump_mysql.la
 
 chown daemon.daemon /opt/lampp/modules/mod_dump_mysql.so
