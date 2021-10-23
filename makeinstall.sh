3325f11c9e8a5312b96aa987c15cffe7abdc4992
阿宝 <talent518@yeah.net>
2021-10-24 01:26:04 +0800

    Fix runTime(unit: second).

--- makeinstall.sh
+++ makeinstall.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/sh
 
-lampp stopapache
-
 rm -rf .libs *.o *.slo *.lo *.la
 
 apxs -cia -L/opt/lampp/lib -I/opt/lampp/include -lmysqlclient mod_dump_mysql.c
 
-chown daemon.daemon /opt/lampp/modules/mod_dump_mysql.so
-
-lampp startapache
